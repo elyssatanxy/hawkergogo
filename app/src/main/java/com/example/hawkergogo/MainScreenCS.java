8065aba0b3efa9ce8f701defe1034d839d52e217
kenthough <111831749+kenthough@users.noreply.github.com>
2023-03-15 18:17:58 +0800

    loading screen

---
+++ app/src/main/java/com/example/hawkergogo/MainScreenCS.java
@@ -0,0 +1,14 @@
+package com.example.hawkergogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreenCS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen_cs);
+    }
+}
