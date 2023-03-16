4ef490670ade7cc0d2c23c32e9e696ff59a2964e
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2023-03-16 15:34:27 +0800

    horizontal scroll

--- app/src/main/java/com/example/hawkergogo/MainActivity.java
+++ app/src/main/java/com/example/hawkergogo/MainActivity.java
@@ -4,21 +4,94 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-Handler handler;
+//    Handler handler;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run(){
+//                Intent intent = new Intent(MainActivity.this, MainScreenCS.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, 3000);
+//    }
+
+
+    // Recycler View object
+    RecyclerView recyclerView;
+
+    // Array list for recycler view data source
+    ArrayList<String> source;
+
+    // Layout Manager
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+
+    // adapter class object
+    Adapter adapter;
+
+    // Linear Layout Manager
+    LinearLayoutManager HorizontalLayout;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run(){
-                Intent intent = new Intent(MainActivity.this, MainScreenCS.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 3000);
+
+        // initialisation with id's
+        recyclerView
+                = (RecyclerView)findViewById(
+                R.id.recycler);
+        RecyclerViewLayoutManager
+                = new LinearLayoutManager(
+                getApplicationContext());
+
+        // Set LayoutManager on Recycler View
+        recyclerView.setLayoutManager(
+                RecyclerViewLayoutManager);
+
+        // Adding items to RecyclerView.
+        AddItemsToRecyclerViewArrayList();
+
+        // calling constructor of adapter
+        // with source list as a parameter
+        adapter = new Adapter(source);
+
+        // Set Horizontal Layout Manager
+        // for Recycler view
+        HorizontalLayout
+                = new LinearLayoutManager(
+                MainActivity.this,
+                LinearLayoutManager.HORIZONTAL,
+                false);
+        recyclerView.setLayoutManager(HorizontalLayout);
+
+        // Set adapter on recycler view
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Function to add items in RecyclerView.
+    public void AddItemsToRecyclerViewArrayList()
+    {
+        // Adding items to ArrayList
+        source = new ArrayList<>();
+        source.add("gfg");
+        source.add("is");
+        source.add("best");
+        source.add("site");
+        source.add("for");
+        source.add("interview");
+        source.add("preparation");
     }
 }
