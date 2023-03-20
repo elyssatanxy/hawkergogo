1b70d8373cd5f5e26d3a0ab63c630e48feb81d70
ara-thana <arathanag.2021@scis.smu.edu.sg>
2023-03-20 15:18:11 +0800

    navbar seller home

--- app/src/main/java/com/example/hawkergogo/SellerHome.java
+++ app/src/main/java/com/example/hawkergogo/SellerHome.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 
 public class SellerHome extends AppCompatActivity{
@@ -92,4 +94,6 @@ public class SellerHome extends AppCompatActivity{
         Intent intent = new Intent(this, Giveaway.class);
         startActivity(intent);
     }
+
+
 }
